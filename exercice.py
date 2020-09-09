180bd2737672ce00cf5b823dba78a2f57f33b0ef
Benjamin De Leener <benjamin.de-leener@polymtl.ca>
2020-09-09 14:56:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,10 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        lettre += 36
+        nombre = 0
+        nombre = ord(lettre)
+        nombre -= 32
+        resultat += chr(nombre)
         resultat += lettre
     return resultat
 
